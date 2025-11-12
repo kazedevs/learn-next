bcf91e2aeef4698cd547e4d831f65906ab99c012
kazedevs <businessrajkumardas@gmail.com>
2025-11-12 19:02:28 +0530

    Update Vercel and Next.js configs, add ESLint config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    // Enable styled-components support
+    styledComponents: true,
+  },
+  images: {
+    domains: ['images.unsplash.com'], // Add any image domains you use
+  },
+  // Remove experimental features that might cause TypeScript errors
 };
 
 export default nextConfig;
